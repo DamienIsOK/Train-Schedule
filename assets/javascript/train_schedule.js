101d944fc840e72996187d611582ebdcdd482717
Damien Wright <damien@Damiens-MacBook-Pro.local>
2016-08-16 21:17:23 -0700

    Finished with JS work

--- assets/javascript/train_schedule.js
+++ assets/javascript/train_schedule.js
@@ -42,27 +42,50 @@ $("#addTrainBtn").on("click", function(){
 	console.log("First Time: " + newTrain.first);
 	console.log("Frequency: " + newTrain.freq);
 
+
+
 	return false;
 });
 
+
 database.ref().on("child_added", function(childSnapshot, prevChildKey){
-	// WHAT IS THIS DOING???
-	// console.log("Child Snapshot Value: " + childSnapshot.val());
+	// What is childSnapshot and prevChild doing? 
+	
+	console.log("Child Snapshot Value: " + childSnapshot.val());
 
 	var newName = childSnapshot.val().name;
 	var newDestination = childSnapshot.val().dest;
 	var newFirstTime = childSnapshot.val().first;
 	var newFrequency = childSnapshot.val().freq;
 
+	console.log('newFirstTime', newFirstTime)
 	console.log("newName: " + newName);
 	console.log("newDestination: " + newDestination);
-	console.log("newFirstTime: " + newFirstTime);
 	console.log("newFrequency: " + newFrequency);
 
-	var convertedFirstTime = moment(new Date(newFirstTime, "hh:mm"));
+	// Why can't I save the following variable to moment().format("hh:mm")
+	var currentTime = moment();
+	console.log(moment(currentTime).format("hh:mm"));
 
-	var nextArrival = moment(convertedFirstTime).diff(moment(), "minutes");
-	console.log("Next Arrival: " + nextArrival);
+	var firstTimeConverted = moment(newFirstTime, "hh:mm").subtract(1, "days");
+	
+	// Get the difference between now and the time of the first train
+	// by subtracting the current time from the first train time
+	timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+	console.log("Difference in time: " + timeDiff);
+
+	// Time apart
+	var remainder = timeDiff % newFrequency;
+	console.log("Remainder: ", remainder);
+
+	// Minutes until the next train calculated by subtracting the the remainder from the frequency
+	var timeUntilTrain = newFrequency - remainder;
+	console.log("Time Til Train: " + timeUntilTrain);
+
+	var nextTrainTime = moment().add(timeUntilTrain, "minutes");
+	console.log("Next arrival: " + moment(nextTrainTime).format("hh:mm"));
+
+	$("#trainTable > tbody").append("<tr><td>" + newName + "</td><td>" + newDestination + "</td><td>" + newFrequency + "</td><td>" + moment(nextTrainTime).format("hh:mm") + "</td><td>" + timeUntilTrain);
 
 	return false;
 });
